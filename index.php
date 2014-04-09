46b5cca05cda390adf1fe64158a6b3b8f9dfbfcb
Elly <eth0eth1@users.noreply.github.com>
2014-04-09 22:38:11 +0100

    Update index.php
    Testing Twitter Bootstrap

--- index.php
+++ index.php
@@ -4,7 +4,18 @@ echo "<h1>Test</h2>"
 
 ?>
 <html>
+
 	<head>
+	<!-- Twitter Bootstrap Test -->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	
 	</head>
 	<body>
 	<h1> This is a test </h1>
