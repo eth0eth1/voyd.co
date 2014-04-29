4f3e68c67b53fa986fc83bdc18c247468690b294
Elly <9212227@gmail.com>
2014-04-29 20:23:34 +0100

    Added exclusions for folders

--- boot.php
+++ boot.php
@@ -1,16 +1,25 @@
 <?php
 #echo "Test";
 
-$dirs = scandir(".");
+# Scan current directory
+# Populate variable with contents
+$in_dir = scandir(".");
 
+# Build list of folders to exclude
+$exclude=array(".","..",".git","font","img","css","js");
+
+
+# Build unordered list of folders
 echo "<ul>";
-
-foreach ($dirs as $scan) {
-  if ( is_dir($scan) ) {
-	echo "<li>$scan</li>";
+foreach ($in_dir as $contents) {
+	
+	# Only display if content is a directory
+	if ( is_dir($contents) ) {
+		if ( !in_array $contents, $exclude ) {
+			echo "<li>$contents</li>";
+		}
 	}
 }
-
 echo "</ul>";
 
 ?>
