a2d3628c92bb8f1903fde19f7e5adb8109520ffc
Elly <9212227@gmail.com>
2014-04-29 22:59:25 +0100

    Test commit

--- boot.php
+++ boot.php
@@ -17,7 +17,7 @@ foreach ($in_dir as $content) {
 	if ( is_dir($content) ) {
 		# Exclude directories within the exclusions
 		if ( !in_array($content, $exclude) ) {
-			print "<li><a href=\"$content\">$content</a></li>";
+			print "<li><a href=\"$content\">$content</a> Test </li>";
 		}
 	}
 }
