80e917e8ecc9a8a41c3d87428b5712d74d81a415
Elly <9212227@gmail.com>
2014-04-30 19:20:47 +0100

    Fill out rest of index page

--- index.php
+++ index.php
@@ -1,9 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+	<h1> Projects </h1>
+</header>
+
+<article>
+<!-- Pull list of projects based on folders -->
 <?php
-
 require "indexer.php";
-
 $indexer = new Indexer;
-
 $indexer->listDirs();
-
 ?>
+</article>
+
+<footer>
+
+<p> Footer </p>
+
+</footer>
+</body>
