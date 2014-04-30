f1da067211211f18f8a5a0d03ccd38c7738e61d1
Elly <9212227@gmail.com>
2014-04-30 19:02:12 +0100

    Add directory scanning to indexer class

--- index.php
+++ index.php
@@ -2,32 +2,8 @@
 
 require "indexer.php";
 
-#print "Test";
-
-# Scan current directory
-# Populate variable with contents
-$in_dir = scandir(".");
-
-# Build list of folders to exclude
-$exclude=array(".","..",".git","font","img","css","js");
-
-
-# Build unordered list of folders
-print "<ul>";
-foreach ($in_dir as $content) {
-	
-	# Only display if content is a directory
-	if ( is_dir($content) ) {
-		# Exclude directories within the exclusions
-		if ( !in_array($content, $exclude) ) {
-			print "<li><a href=\"$content\">$content</a></li>";
-		}
-	}
-}
-print "</ul>";
-
 $indexer = new Indexer;
 
-$indexer->sayTesting();
+$indexer->listDirs();
 
 ?>
--- indexer.php
+++ indexer.php
@@ -1,11 +1,31 @@
 <?php
 class Indexer {
 
-	private $test = "Testing";
-	
-	public function sayTesting() {
-		echo $this->test;
+	# Scan current directory
+	# Populate variable with contents
+	private $in_dir = scandir(".");
+
+	# Build list of folders to exclude
+	private $exclude=array(".","..",".git","font","img","css","js");
+
+
+	public function listDirs {
+		# Build unordered list of folders
+		echo "<ul>";
+		foreach ($in_dir as $content) {
+			
+			# Only display if content is a directory
+			if ( is_dir($content) ) {
+				# Exclude directories within the exclusions
+				if ( !in_array($content, $exclude) ) {
+					# Echo as list item hyperlinks
+					echo "<li><a href=\"$content\">$content</a></li>";
+				}
+			}
+		}
+		echo "</ul>";
 	}
+
 }
 
 ?>
