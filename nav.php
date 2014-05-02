38e88930e43e79fe5bc2c73831c1a5655a4fd88d
Elly <9212227@gmail.com>
2014-05-02 19:14:56 +0100

    Added testing for new objects

---
+++ nav.php
@@ -0,0 +1,15 @@
+<?php 
+
+# Construct:
+#  - Pass array as menu items
+# - Pass header
+ 
+class Nav {
+
+	function __construct() {
+		echo "Test";
+	}
+
+}
+ 
+?>
---
+++ test.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+	<head>
+	</head>
+	<body>
+		<p> Test </p>
+		<?php
+			require nav.php;
+			$navbar = new Nav();
+		?>
+	</body>
+</html>
