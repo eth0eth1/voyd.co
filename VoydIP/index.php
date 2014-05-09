08c5dabd987563ce17e688bbb4989e3b3815ed02
Elly <9212227@gmail.com>
2014-05-09 19:57:39 +0100

    Adding container to fox an speak

--- VoydIP/index.php
+++ VoydIP/index.php
@@ -4,8 +4,10 @@
 	<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-	<div id="fox"> <img src="img/fox.png">
-	<h1 id="speak"> Yap! <?php echo $_SERVER['REMOTE_ADDR'];?> </h1>
+	<div class="container">
+		<div id="fox"> <img src="img/fox.png">
+		<h1 id="speak"> Yap! <?php echo $_SERVER['REMOTE_ADDR'];?> </h1>
+		</div>
 	</div>
 	</body>
 </html>
