a086a08e8488f3da1a313c4431b245e19936b325
Elly <9212227@gmail.com>
2014-05-09 20:04:15 +0100

    Fix container bug

--- VoydIP/index.php
+++ VoydIP/index.php
@@ -5,9 +5,8 @@
 	</head>
 	<body>
 	<div class="container">
-		<div id="fox"> <img src="img/fox.png">
+		<img id="fox" src="img/fox.png">
 		<h1 id="speak"> Yap! <?php echo $_SERVER['REMOTE_ADDR'];?> </h1>
-		</div>
 	</div>
 	</body>
 </html>
