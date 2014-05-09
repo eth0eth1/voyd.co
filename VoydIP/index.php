96476d9d37aa7c9e6899ecdb94e56b0ba00da3de
Elly <9212227@gmail.com>
2014-05-09 22:57:57 +0100

    Change container to exclude image

--- VoydIP/index.php
+++ VoydIP/index.php
@@ -5,10 +5,11 @@
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300">
 	</head>
 	<body>
-	<div class="container">
+	<div class="speak">
 		<h1 id="yap"> Yap! </h1>
 		<h1 id="ip"> <?php echo $_SERVER['REMOTE_ADDR'];?> </h1>
-		<img id="fox" src="img/fox.png">
 	</div>
+	<img id="fox" src="img/fox.png">
+	
 	</body>
 </html>
