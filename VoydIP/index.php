c9b13c8b0d69595140631e144c56c3d6ce77eaf0
Elly <9212227@gmail.com>
2014-05-09 19:55:45 +0100

    Add fox picture and styles

--- VoydIP/index.php
+++ VoydIP/index.php
@@ -4,6 +4,8 @@
 	<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-	<h1> Yap! <?php echo $_SERVER['REMOTE_ADDR'];?> </h1>
+	<div id="fox"> <img src="img/fox.png">
+	<h1 id="speak"> Yap! <?php echo $_SERVER['REMOTE_ADDR'];?> </h1>
+	</div>
 	</body>
 </html>
