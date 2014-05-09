8a3907343e77eb822c3d39ff5b8bcdbbf6d83fe1
Elly <9212227@gmail.com>
2014-05-09 23:22:23 +0100

    Try and fix layout with container div

--- VoydIP/index.php
+++ VoydIP/index.php
@@ -5,11 +5,12 @@
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300">
 	</head>
 	<body>
-	<div class="speak">
+	<div class="container">
 		<h1 id="yap"> Yap! </h1>
 		<h1 id="ip"> <?php echo $_SERVER['REMOTE_ADDR'];?> </h1>
+		<img id="fox" src="img/fox.png">
 	</div>
-	<img id="fox" src="img/fox.png">
+	
 	
 	</body>
 </html>
