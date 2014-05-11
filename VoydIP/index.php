858cdd8f909f98da64c04b6c635da908739d9624
Elly <9212227@gmail.com>
2014-05-11 17:43:12 +0100

    Remove extras

--- VoydIP/index.php
+++ VoydIP/index.php
@@ -5,12 +5,6 @@
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300">
 	</head>
 	<body>
-	<div class="container">
-		<h1 id="yap"> Yap! </h1>
-		<h1 id="ip"> <?php echo $_SERVER['REMOTE_ADDR'];?> </h1>
-		<img id="fox" src="img/fox.png">
-	</div>
-	
 	
 	</body>
 </html>
