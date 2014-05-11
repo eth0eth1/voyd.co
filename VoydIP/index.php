a905bc16a26f17a755637dea14b86c3dec7c96db
Elly <9212227@gmail.com>
2014-05-11 18:03:18 +0100

    Create gradually loading scheme

--- VoydIP/index.php
+++ VoydIP/index.php
@@ -5,6 +5,8 @@
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300">
 	</head>
 	<body>
-	<!--<img id="fox" src="img/fox.png">-->
+	<div id="overlay">
+		<!--<img id="fox" src="img/fox.png">-->
+	</div>
 	</body>
 </html>
