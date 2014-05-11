1aefc09024f4434e7d0a4e7cbde19ced2cd721f8
Elly <9212227@gmail.com>
2014-05-11 17:53:29 +0100

    Remove extras

--- VoydIP/index.php
+++ VoydIP/index.php
@@ -5,6 +5,6 @@
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300">
 	</head>
 	<body>
-	<img id="fox" src="img/fox.png">
+	<!--<img id="fox" src="img/fox.png">-->
 	</body>
 </html>
