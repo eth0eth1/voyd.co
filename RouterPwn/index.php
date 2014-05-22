6353c7a83467a237728289b8b92850f328f010d8
Elly <9212227@gmail.com>
2014-05-22 20:56:20 +0100

    Added import java.net.InetAddress

--- RouterPwn/index.php
+++ RouterPwn/index.php
@@ -1,4 +1,5 @@
 <html><head><script type='text/javascript'>
+import java.net.InetAddress;
 var yip2=java.net.InetAddress.getLocalHost();	
 var yip=yip2.getHostAddress();
 alert("your machine's local network IP is "+yip);
