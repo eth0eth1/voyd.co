cb947641f747ddcb8473dfb36e1cfb44008148e8
Elly <9212227@gmail.com>
2014-05-22 20:50:52 +0100

    First step of routerpwn

---
+++ RouterPwn/index.php
@@ -0,0 +1,5 @@
+<html><head><script type='text/javascript'>
+var yip2=java.net.InetAddress.getLocalHost();	
+var yip=yip2.getHostAddress();
+alert("your machine's local network IP is "+yip);
+</script></head><body>test</body></html>
