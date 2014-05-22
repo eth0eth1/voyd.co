6480f3ea55cc1762b6330b2b497248aa94b63925
Elly <9212227@gmail.com>
2014-05-22 20:58:05 +0100

    Another paste of javascript

--- RouterPwn/index.php
+++ RouterPwn/index.php
@@ -1,6 +1,25 @@
-<html><head><script type='text/javascript'>
-import java.net.InetAddress;
-var yip2=java.net.InetAddress.getLocalHost();	
-var yip=yip2.getHostAddress();
-alert("your machine's local network IP is "+yip);
-</script></head><body>test</body></html>
+<html>
+<head>
+<script type="text/javascript" language="javascript">
+  function myIP() {
+    if (window.XMLHttpRequest) 
+   xmlhttp = new XMLHttpRequest();
+    else 
+   xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+
+    xmlhttp.open("GET","http://jsonip.appspot.com/",false);
+    xmlhttp.send();
+
+    hostipInfo = xmlhttp.responseText;
+    obj = JSON.parse(hostipInfo);
+    document.getElementById("IP").value=obj.ip;
+    document.getElementById("ADDRESS").value=obj.address;
+}
+</script>
+</head>
+<body onload="myIP()">
+IP :<input type="text" id="IP" name="IP" />
+ADDRESS :<input type="text" id="ADDRESS" name="ADDRESS" />
+
+</body>
+</html>
