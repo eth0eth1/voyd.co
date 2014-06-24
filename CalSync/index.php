98d192d947929a4c1f8588dcbd6dee0d6378137b
Elly <9212227@gmail.com>
2014-06-24 22:29:33 +0100

    Add more testing files

--- CalSync/index.php
+++ CalSync/index.php
@@ -2,8 +2,18 @@
 require_once 'Google/Client.php';
 require_once 'Google/Service/Calendar.php';
 
+$apiClient = new apiClient();
+$apiClient->setUseObjects(true);
+$service = new apiCalendarService($apiClient);
 
 $calendarListEntry = $service->calendarList->get('calendarId');
+
+if (isset($_SESSION['oauth_access_token'])) {
+  $apiClient->setAccessToken($_SESSION['oauth_access_token']);
+} else {
+  $token = $apiClient->authenticate();
+  $_SESSION['oauth_access_token'] = $token;
+}
 
 echo $calendarListEntry->getSummary();
 
